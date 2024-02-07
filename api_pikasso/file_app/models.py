2059b18fe0fb6e9cec33f9b5a87af11f8087e966
Dan <dan.berdinskikh@mail.ru>
2024-02-07 21:54:14 +0300

    Start project and app, made model and serializer

---
+++ api_pikasso/file_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class File(models.Model):
+    file = models.FileField(
+        verbose_name='Файл',
+        upload_to='files/',
+        )
+    upload_at = models.DateTimeField(
+        verbose_name='Время загрузки',
+        auto_now_add=True,
+        )
+    processed = models.BooleanField(
+        verbose_name='Статус загрузки',
+        default=False,
+        )
