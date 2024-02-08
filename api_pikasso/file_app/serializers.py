53ce17c73fa0485b0bede94b78aaccae5c47be86
Dan <dan.berdinskikh@mail.ru>
2024-02-08 15:36:27 +0300

    Split serializer

--- api_pikasso/file_app/serializers.py
+++ api_pikasso/file_app/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from .models import File
 
 
-class FileSerializer(serializers.ModelSerializer):
+class CreateFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = ('file',)
+
+
+class ListFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('file', 'upload_at', 'processed',)
